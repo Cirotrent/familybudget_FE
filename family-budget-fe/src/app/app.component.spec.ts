0564315d5f8a483bf595c0ec94d9b3f35672e173
CiroTrentacosti <c.trentacosti.esterno@almaviva.it>
2026-04-15 09:38:02 +0200

    cacellazionne e reinstallazione

--- family-budget-fe/src/app/app.component.spec.ts
+++ family-budget-fe/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'familybudget_FE' title`, () => {
+  it(`should have the 'family-budget-fe' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('familybudget_FE');
+    expect(app.title).toEqual('family-budget-fe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, familybudget_FE');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, family-budget-fe');
   });
 });
